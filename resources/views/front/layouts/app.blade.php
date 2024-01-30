2d1b4cfa49b40f94f5658b2f1a6185944493ffe3
Ishant2110 <153583453+Ishant2110@users.noreply.github.com>
2024-01-30 15:53:57 +0000

    Title change

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
 <head>
     <!-- Basic need -->
-    <title>Open Pediatrics</title>
+    <title>myshowdb</title>
     <meta charset="UTF-8">
     <meta name="description" content="">
     <meta name="keywords" content="">
